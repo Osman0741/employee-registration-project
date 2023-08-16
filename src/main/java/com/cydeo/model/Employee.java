f90e567d418101b671a4035da70d419b2b20e6a4
LENOVO <osmnozdmrosh@gmail.com>
2023-08-16 18:10:58 +0300

    Completed Employee Registration

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,6 +1,7 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 @Getter
@@ -14,6 +15,8 @@ public class Employee {
 
     private String firstName;
     private String lastName;
+    //Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
     private String password;
