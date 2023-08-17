53aebd7271fddbdde7e3a852310ec8eb464e7348
LENOVO <osmnozdmrosh@gmail.com>
2023-08-17 10:32:18 +0300

    added backend validation

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,10 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 @Getter
 @Setter
@@ -13,6 +17,15 @@ import java.time.LocalDate;
 
 public class Employee {
 
+    //@NotNull   Field shouldn't be null
+  //  @NotEmpty   Field shouldn't be ""
+  //  @NotBlank    Field shouldn't be "           "
+
+
+
+
+    @NotBlank
+    @Size(max = 12, min =2)
     private String firstName;
     private String lastName;
     //Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy
